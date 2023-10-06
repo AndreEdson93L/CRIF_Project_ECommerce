0890295bf2c96db8192da94a454568c9511c6de2
Lorenzoni Andrea Edson <a.lorenzoni@crif.com>
2023-10-06 16:40:41 +0200

    Implemented login message (successfull && error) while trying to login.

--- frontend/src/app/components/noAuth/official-login/official-login.component.ts
+++ frontend/src/app/components/noAuth/official-login/official-login.component.ts
@@ -10,40 +10,51 @@ import { LoginService } from 'src/app/services/login.service';
 export class OfficialLoginComponent {
   username: string = '';
   password: string = '';
+  successMessage: string = '';
+  errorMessage: string = '';
 
   constructor(private loginService: LoginService,
     private router : Router) {}
 
   //component method used to call [login of auth service]
   login(): void {
-    console.log("Login Init Funz: ");
+    console.log("Login function called");
     
     let hasBeeenSucces = false
     this.loginService.login(this.username, this.password).subscribe({
       next: (data) => {
-        localStorage.clear()
-        
+        localStorage.clear();
         localStorage.setItem('jwtToken', JSON.stringify(data));
+
+        this.successMessage = "Login successful.. Redirecting!";
         
         const helloJwt = localStorage.getItem('jwtToken');
-        console.log(helloJwt);
-        hasBeeenSucces = true
+        console.log({"jwtToken: ": helloJwt});
+        hasBeeenSucces = true;
 
-        console.log("Set to true var true login: ", hasBeeenSucces);
-        
+        console.log("hasBeenSuccess: ", hasBeeenSucces);
 
         if(hasBeeenSucces){
-          console.log("Inside route redirect login... ");
-          
-          this.router.navigate(["/user-details"])
-        }/* else{
-          this.router.navigate(["/gandalf-page"])
-        } */  
+          setTimeout(() => {
+            this.router.navigate(["/user-details"])
+          }, 3000);
+        }   
       },
+
       error: (err) => {
-        console.log('You should not pass!', err);
+        console.log('You shall not pass! ');
+
+        // Check the error response from the server, if available
+        console.log("err: ", err);
+        console.log("err.error: ", err.error);
+        console.log("err.error.message: ", err.error.message);
+        
+        this.errorMessage = "Login failed. Please try again.";
+        
+        setTimeout(() => {
+          this.errorMessage = '';
+        }, 5000);
       },
     });
-    
   }
 }
